b7114e826392c6485edad9add32a94fe3e645580
adhis <tiva.adhisti@ui.ac.id>
2024-04-17 18:34:39 +0700

    Implement subscribe function in Notification controller.

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -8,6 +8,6 @@ pub fn route_stage() -> AdHoc {
         rocket
             .mount("/product", routes![product::create, product::list, 
                 product::read, product::delete, product::publish])
-            .mount("/notification", routes![])
+            .mount("/notification", routes![notification::subscribe])
     });
 }
